766c4bfbe6f3cc772ae3378760b354c213afa457
LorenzoFernandezA <lfernandezarancibia@udesa.edu.ar>
2025-05-19 17:42:19 -0300

    empezamos con el register

--- proyectoIntegrador/controllers/rutaCompartidacontroller.js
+++ proyectoIntegrador/controllers/rutaCompartidacontroller.js
@@ -1,3 +1,8 @@
+const {where} = require("sequelize");
+let db = require("../database/models");
+let bcrypt = require ('bcryptjs');
+
+
 const rutaCompartidacontroller={
     register:function(req,res){
         res.render('register', { });
@@ -5,6 +10,22 @@ const rutaCompartidacontroller={
     login:function(req,res){
         res.render('login', { });
     },
+    create:function(req,res){
+        const usuario = req.body.usuario;
+        const email = req.body.email;
+        const password = req.body.password;
+        db.index.findeOne({where:{email}
+        }).then(function(resultado){
+            if(resultado){
+                return res.send("El usuario ya existe")
+            }else if (resultado == null){
+                return res.send("El usuario no existe")
+            }else{
+                return res.redirect("/")
+            }
+        
+        })
+    }
     
 }
 
--- proyectoIntegrador/routes/rutaCompartida.js
+++ proyectoIntegrador/routes/rutaCompartida.js
@@ -4,6 +4,6 @@ var rutaCompartidacontroller = require('../controllers/rutaCompartidacontroller'
 
 router.get('/register', rutaCompartidacontroller.register);
 router.get('/login', rutaCompartidacontroller.login);
-
+router.get('/newuser', rutaCompartidacontroller.create)
 
 module.exports = router;
