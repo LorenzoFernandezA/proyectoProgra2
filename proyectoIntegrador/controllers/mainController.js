b71b25976850618476009b2abff091dfe5a8f73c
frepett <frepettoechave@udesa.edu.ar>
2025-06-02 11:57:49 -0300

    lo de la clase

--- proyectoIntegrador/controllers/mainController.js
+++ proyectoIntegrador/controllers/mainController.js
@@ -3,8 +3,9 @@ const db = require('../database/models')
 producto = base.productos
 const mainController={
     index: function (req, res) {
-        include:[{association:"user"}]
-        res.render('index', {productos:db.Producto});
-    }
-}
+        db.Producto.findAll({
+            include:[{association:"user"}]
+        }).then(function(productos){
+            res.render('index', {productos:productos});
+        })}}
 module.exports = mainController;
